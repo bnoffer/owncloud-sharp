1486ef0ddb5d10434e4e1c9179d59f42db95c717
Bastian Noffer <b.noffer@mac.com>
2015-09-10 23:06:53 +0200

    Initial commit

---
+++ owncloud-sharp/OwnCloud.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace owncloudsharp
+{
+	public class ResponseError : Exception {
+		
+		public ResponseError() : base() {
+			
+		}
+	}
+}
+
